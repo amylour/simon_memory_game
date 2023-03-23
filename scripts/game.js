66eb5cf9f9234f047c4c84f58c5ac1e7f7211421
Amy Richardson <amyloumc@gmail.com>
2023-03-23 22:26:12 +0000

    Simen game practice - add functions and tests

---
+++ scripts/game.js
@@ -0,0 +1,46 @@
+let game = {
+    score: 0,
+    currentGame: [],
+    playerMoves: [],
+    choices: ["button1", "button2", "button3", "button4"],
+}
+
+function newGame() {
+    game.score = 0;
+    game.currentGame = [];
+    game.playerMoves = [];
+    showScore();
+    addTurn();
+}
+
+function addTurn() {
+    game.playerMoves = [];
+    game.currentGame.push(game.choices[(Math.floor(Math.random() * 4))]);
+    showTurns();
+}
+
+function showScore() {
+    document.getElementById("score").innerText = game.score;
+}
+
+function lightsOn(circ) {
+    document.getElementById(circ).classList.add(circ + "light");
+    setTimeout(function() {
+        document.getElementById(circ).classList.remove(circ + "light");
+    }, 400);
+
+}
+
+function showTurns() {
+    game.turnNumber = 0;
+    let turns = setInterval(function() {
+        lightsOn(game.currentGame[game.turnNumber]);
+        game.turnNumber++;
+        if (game.turnNumber >= game.currentGame.lenght) {
+            clearInterval(turns);
+        }
+    }, 800);
+}
+
+
+module.exports = { game, newGame, showScore, addTurn, lightsOn, showTurns };
